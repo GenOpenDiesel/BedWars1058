d00596e786adea8f0865d56781ae79510fdd5d9c
GenOpenDiesel <83091816+GenOpenDiesel@users.noreply.github.com>
2026-01-23 12:49:23 +0100

    Update GameEndListener.java

--- bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/GameEndListener.java
+++ bedwars-plugin/src/main/java/com/andrei1058/bedwars/listeners/GameEndListener.java
@@ -1,20 +1,15 @@
 package com.andrei1058.bedwars.listeners;
 
-import com.andrei1058.bedwars.BedWars;
 import com.andrei1058.bedwars.api.events.gameplay.GameEndEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.UUID;
 
 public class GameEndListener implements Listener {
@@ -25,25 +20,12 @@ public class GameEndListener implements Listener {
             return;
         }
 
-        // Przygotowanie przedmiotu "Rematch"
-        ItemStack rematchItem = BedWars.nms.createItemStack(Material.PAPER.name(), 1, (short) 0);
-        ItemMeta meta = rematchItem.getItemMeta();
-        if (meta != null) {
-            meta.setDisplayName("§aZagraj ponownie §7(Prawy klik)");
-            meta.setLore(Arrays.asList("§7Kliknij, aby dołączyć", "§7do nowej gry!"));
-            rematchItem.setItemMeta(meta);
-        }
-        // Dodajemy tag REMATCH_<nazwaGrupy>
-        rematchItem = BedWars.nms.addCustomData(rematchItem, "REMATCH_" + event.getArena().getGroup());
-
-        // Czyść ekwipunek i daj przedmiot wszystkim graczom w arenie
-        for (Player p : event.getArena().getPlayers()) {
-            p.getInventory().clear();
-            p.getInventory().setItem(4, rematchItem); // Slot 4 (środek paska)
-            p.updateInventory();
+        // clear inventories
+        for (UUID p : event.getAliveWinners()) {
+            Bukkit.getPlayer(p).getInventory().clear();
         }
 
-        // Usuń leżące przedmioty
+        // clear dropped items
         World game = event.getArena().getWorld();
         for (Entity item : game.getEntities()) {
             if (item instanceof Item || item instanceof ItemStack){
